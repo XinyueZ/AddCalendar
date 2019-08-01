3edd1e195d9727ae9a8b7389a417dfce01fec760
XinyueZ <dev.xinyue.zhao@gmail.com>
2019-08-01 20:55:01 +0200

    Init commit
    - Setup app version, compile-version, SDK
    - Firebase ML Kit setup
    - ktlint setup
    - Dependencies setup
    - Temp remove all tests directories
    - gitignore setup

---
+++ app/src/main/java/io/add/calendar/MainActivity.kt
@@ -0,0 +1,24 @@
+package io.add.calendar
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        handleIntent(intent)
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        handleIntent(intent)
+    }
+
+    private fun handleIntent(intent: Intent?) {
+
+    }
+}
