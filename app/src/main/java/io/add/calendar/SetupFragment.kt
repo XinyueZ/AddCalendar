c297efb0d2bfc7dacb4ca61004319eba741b014e
ChrisDEV <7869833+XinyueZ@users.noreply.github.com>
2019-08-05 13:12:54 +0200

    Release version 0.0.2

--- app/src/main/java/io/add/calendar/SetupFragment.kt
+++ app/src/main/java/io/add/calendar/SetupFragment.kt
@@ -5,10 +5,12 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.observe
 import io.add.calendar.databinding.SetupFragmentBinding
+import io.add.calendar.extensions.shareCompat
+import io.add.calendar.utils.EventObserver
 import io.add.calendar.viewmodels.SetupViewModel
 import io.add.calendar.viewmodels.SetupViewModelFactory
 
@@ -35,8 +37,14 @@ class SetupFragment : Fragment() {
     }
 
     private fun subscribeUi() {
-        viewModel.onSetupCompleted.observe(viewLifecycleOwner) {
+        viewModel.echo.observe(viewLifecycleOwner, EventObserver { echoMsg ->
+            Toast.makeText(requireContext(), echoMsg, Toast.LENGTH_LONG).show()
+        })
+        viewModel.onSetupCompleted.observe(viewLifecycleOwner, EventObserver {
             requireActivity().finish()
-        }
+        })
+        viewModel.onShareApp.observe(viewLifecycleOwner, EventObserver { shareText ->
+            requireActivity().shareCompat(shareText)
+        })
     }
 }
--- app/src/main/java/io/add/calendar/viewmodels/SetupViewModel.kt
+++ app/src/main/java/io/add/calendar/viewmodels/SetupViewModel.kt
@@ -8,17 +8,25 @@ import androidx.lifecycle.MutableLiveData
 import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslateLanguage
 import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslateModelManager
 import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslateRemoteModel
+import io.add.calendar.R
 import io.add.calendar.utils.Event
 import java.util.Locale
 
 class SetupViewModel(app: Application) : AndroidViewModel(app) {
     val setupInProgress = ObservableBoolean(false)
 
+    private val _echo = MutableLiveData<Event<Int>>()
+    val echo: LiveData<Event<Int>> = _echo
+
     private val _onSetupCompleted = MutableLiveData<Event<Unit>>()
     val onSetupCompleted: LiveData<Event<Unit>> = _onSetupCompleted
 
+    private val _onShareApp = MutableLiveData<Event<String>>()
+    val onShareApp: LiveData<Event<String>> = _onShareApp
+
     fun setup() {
-        if (setupInProgress.get()) return //Ignore any intercept while setup is in progress.
+        _echo.value = Event(R.string.setup_in_progress_echo)
+        if (setupInProgress.get()) return // Ignore any intercept while setup is in progress.
 
         setupStart()
         getModels()
@@ -48,11 +56,16 @@ class SetupViewModel(app: Application) : AndroidViewModel(app) {
     }
 
     private fun setupStart() {
+        _echo.value = Event(R.string.setup_in_progress_echo)
         setupInProgress.set(true)
     }
 
     private fun setupCompleted() {
         setupInProgress.set(false)
         _onSetupCompleted.value = Event(Unit)
+    }
+
+    fun shareApp(shareText: String) {
+        _onShareApp.value = Event(shareText)
     }
 }
