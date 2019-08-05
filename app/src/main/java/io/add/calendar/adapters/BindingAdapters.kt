4f47b76898b8c38bcf29255dc9d1514ca5eff689
XinyueZ <dev.xinyue.zhao@gmail.com>
2019-08-05 13:10:45 +0200

    Fix #7

--- app/src/main/java/io/add/calendar/adapters/BindingAdapters.kt
+++
@@ -1,9 +0,0 @@
-package io.add.calendar.adapters
-
-import androidx.databinding.BindingAdapter
-import com.chinalwb.are.AREditor
-
-@BindingAdapter("editorContent")
-fun AREditor.editorContent(editorContent: String?) {
-    editorContent?.let { str -> this.fromHtml(str) }
-}
