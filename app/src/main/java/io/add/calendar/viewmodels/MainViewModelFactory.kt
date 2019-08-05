18a0780fd7f8bae201ea324326cfafe34928a5f0
ChrisDEV <7869833+XinyueZ@users.noreply.github.com>
2019-08-05 21:46:46 +0200

    Fix #14 (#15)

--- app/src/main/java/io/add/calendar/viewmodels/MainViewModelFactory.kt
+++ app/src/main/java/io/add/calendar/viewmodels/MainViewModelFactory.kt
@@ -3,10 +3,11 @@ package io.add.calendar.viewmodels
 import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import io.add.calendar.domain.AppStartChecker
 
 class MainViewModelFactory(app: Application) :
     ViewModelProvider.AndroidViewModelFactory(app) {
-    private val viewModel = MainViewModel(app)
+    private val viewModel = MainViewModel(app, AppStartChecker(app))
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return viewModel as T
