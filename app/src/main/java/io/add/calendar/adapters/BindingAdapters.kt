4c94922744bb9232a0d93dc09fb1d4adbc59129c
XinyueZ <dev.xinyue.zhao@gmail.com>
2019-08-06 22:29:26 +0200

    resolve polishing on video-view

--- app/src/main/java/io/add/calendar/adapters/BindingAdapters.kt
+++ app/src/main/java/io/add/calendar/adapters/BindingAdapters.kt
@@ -2,6 +2,7 @@ package io.add.calendar.adapters
 
 import android.graphics.drawable.AnimatedVectorDrawable
 import android.net.Uri
+import android.view.View
 import android.widget.ImageView
 import android.widget.MediaController
 import android.widget.VideoView
@@ -10,12 +11,16 @@ import androidx.databinding.BindingAdapter
 @BindingAdapter("src")
 fun VideoView.setSource(filename: String) {
     val srcUri = Uri.parse("android.resource://${context.packageName}/raw/$filename")
-    setMediaController(MediaController(context))
+    setMediaController(MediaController(context).apply { this.visibility = View.GONE })
     setVideoURI(Uri.parse(srcUri.toString()))
     seekTo(0)
     requestFocus()
-    setOnPreparedListener { it.isLooping = true }
-    start()
+    setOnPreparedListener {
+        it.isLooping = true
+        it.setVolume(0f, 0f)
+        it.isLooping = true
+        start()
+    }
 }
 
 @BindingAdapter("startAnimation")
