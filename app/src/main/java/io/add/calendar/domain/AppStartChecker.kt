d2d11f9a00decdb78d000eeda651be20e7bc9ba6
XinyueZ <dev.xinyue.zhao@gmail.com>
2019-08-13 19:59:10 +0200

    extract interface to single file, update readme

---
+++ app/src/main/java/io/add/calendar/domain/AppStartChecker.kt
@@ -0,0 +1,30 @@
+package io.add.calendar.domain
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.core.content.edit
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import io.add.calendar.utils.Event
+
+const val IS_FIRST_LAUNCHED = "io.add.calendar.domain.AppStartChecker.IS_FIRST_LAUNCHED"
+
+class AppStartChecker(context: Context) : IAppStartChecker {
+    private val preferences: SharedPreferences =
+        context.getSharedPreferences(IS_FIRST_LAUNCHED, Context.MODE_PRIVATE)
+
+    private val _navigateToAppSetup = MutableLiveData<Event<Unit>>()
+    override val navigateToAppSetup: LiveData<Event<Unit>> = _navigateToAppSetup
+
+    override var isFirstLaunched: Boolean
+        get() = preferences.getBoolean(IS_FIRST_LAUNCHED, true)
+        set(value) {
+            preferences.edit {
+                putBoolean(IS_FIRST_LAUNCHED, value)
+            }
+        }
+
+    override fun gotoAppSetup() {
+        _navigateToAppSetup.value = Event(Unit)
+    }
+}
